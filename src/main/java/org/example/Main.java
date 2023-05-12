794d6fd818f48ae655ae6bb6a538d3001cae38a4
PeterFlandin <peterflandin@gmail.com>
2023-05-12 11:56:54 +0200

    connection base de donnee

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,21 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException{
+
+
+
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/movies", "root", "246810");
+
+
+        System.out.println("connecté à la base de donnée !!!");
     }
+
+
+
 }
