0f02498dac0a5a19885172942925de43245b8f7a
PeterFlandin <peterflandin@gmail.com>
2023-05-12 12:35:48 +0200

    connection base de donnee ajout utilisateur et droit suur bdd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,17 @@ import java.sql.SQLException;
 
 public class Main {
 
+    private static final String url=  "jdbc:mysql://localhost:3306/monlivre?allowPublicKeyRetrieval=true&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris";
+    private static final String password= "246810";
+    private static final String username = "app_test";
+
+
+
+
     public static void main(String[] args) throws SQLException{
 
 
-
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/movies", "root", "246810");
+        Connection conn = DriverManager.getConnection(url, username, password);
 
 
         System.out.println("connecté à la base de donnée !!!");
